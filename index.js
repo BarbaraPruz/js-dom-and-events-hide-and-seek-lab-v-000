790ce3a24362674fcca10b8be6343862e9659ce6
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-07 05:29:17 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function getChild(element) {
+  let child = element.querySelector(':first-child');
+  if (child != null)
+    return getChild(child);
+  return element;
+}
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+  return getChild(grandNode);
+}
