fd15da55678b2e97df7709a7852c53dd591a3904
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-08 01:26:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function getChild(element) {
-  let child = element.querySelector(':first-child');
-  if (child != null)
-    return getChild(child);
-  return element;
-}
+// function getChild(element) {
+//   let child = element.querySelector(':first-child');
+//   if (child != null)
+//     return getChild(child);
+//   return element;
+// }
+// function deepestChild() {
+//   let grandNode = document.querySelector('#grand-node');
+//   return getChild(grandNode);
+// }
 function deepestChild() {
-  let grandNode = document.querySelector('#grand-node');
-  return getChild(grandNode);
-}
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
 
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
 function increaseRankBy(n) {
   // all li's in rankedList classes
   let rankedList = document.querySelectorAll('.ranked-list li');
@@ -24,3 +34,14 @@ function increaseRankBy(n) {
     rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML,10) + n;
   }
 }
+// function increaseRankBy(n) {
+//   const rankedLists = document.querySelectorAll('.ranked-list')
+//
+//   for (let i = 0, l = rankedLists.length; i < l; i++) {
+//     let children = rankedLists[i].children
+//
+//     for (let j = 0, k = children.length; j < k; j++) {
+//       children[j].innerHTML = parseInt(children[j].innerHTML) + n
+//     }
+//   }
+// }
