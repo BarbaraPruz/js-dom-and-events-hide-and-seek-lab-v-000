28002c9aea7c6fa771580650fcb76720cf452b2f
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-08 01:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function getChild(element) {
-  let child = element.querySelector(':first-child');
-  if (child != null)
-    return getChild(child);
-  return element;
-}
+// function getChild(element) {
+//   let child = element.querySelector(':first-child');
+//   if (child != null)
+//     return getChild(child);
+//   return element;
+// }
+// function deepestChild() {
+//   let grandNode = document.querySelector('#grand-node');
+//   return getChild(grandNode);
+// }
 function deepestChild() {
-  let grandNode = document.querySelector('#grand-node');
-  return getChild(grandNode);
-}
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
 
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
 function increaseRankBy(n) {
   // all li's in rankedList classes
   let rankedList = document.querySelectorAll('.ranked-list li');
